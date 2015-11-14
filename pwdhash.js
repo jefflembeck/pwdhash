34d8c30266d1aa16cd580785711af0ff3bf3ba09
Jeffrey Lembeck <jlembeck@gmail.com>
2015-11-13 22:31:06 -0800

    First run of nodeifying this stuff

---
+++ pwdhash.js
@@ -0,0 +1,37 @@
+/*
+ * Remote PwdHash
+ * A JavaScript implementation of the PwdHash hashing algorithm.
+ * Version 1.0 Copyright (C) Stanford University 2004-2006
+ * Author: Collin Jackson
+ * Other Contributors: Dan Boneh, John Mitchell, Nick Miyake, and Blake Ross
+ * Distributed under the BSD License
+ * See http://crypto.stanford.edu/PwdHash for more info.
+ * Requires the Javascript MD5 library, available here: http://pajhome.org.uk/crypt/md5
+ */
+
+/*
+ * Initialize page with default hashing parameters.
+ */
+
+var SPH_DomainExtractor = require('./lib/domain-extractor');
+var SPH_HashedPassword = require('./lib/hashed-password');
+var SPH_kPasswordPrefix = "@@";
+
+/*
+ * Returns a conforming hashed password generated from the form's field values.
+ */
+function Generate(uri, data)
+{
+  var domain = (new SPH_DomainExtractor()).extractDomain(uri);
+  var size = SPH_kPasswordPrefix.length;
+  if (data.substring(0, size) == SPH_kPasswordPrefix)
+    data = data.substring(size);
+  var result = "" + new SPH_HashedPassword(data, domain);
+  return result;
+}
+
+
+var uri = process.argv[2];
+var pwd = process.argv[3];
+
+console.log(Generate(uri, pwd));
